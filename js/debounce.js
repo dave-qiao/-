cd85c6b160e0299255914a562029da88421d111f
无尘 <wuchen@helijia.com>
2024-03-07 22:20:24 +0800

    feat: 防抖函数实现(debounce)

--- js/debounce.js
+++ js/debounce.js
@@ -1,20 +1,38 @@
-function debounce(func, time = 300) {
-  if (typeof func === 'function' && typeof time === 'number') {
-    console.log(time, 'time');
-    let timer = null;
-    return function () {
+// 防抖函数: 固定时间内只执行一次函数，重复调用会重置计时的开始时间，最后一个调用生效并执行
+
+/**
+ * 简单版本
+ * @param {Function} fn 
+ * @param {Number} timeout
+ * @param {Bollean} immediate
+ * @returns
+ */
+
+function debounce(fn, timeout, immediate = false) {
+  // 参数校验
+  if (typeof fn !== 'function' || typeof timeout !== 'number') {
+    console.error('参数类型异常');
+    return;
+  }
+
+  let timer = null;
+  return function () {
+    if (timer) {
       clearTimeout(timer);
+    }
+    // 是否立即执行
+    if (immediate) {
+      !timer && fn.apply(this, arguments);
       timer = setTimeout(() => {
-        func.apply(this, arguments);
-      }, time);
-    };
-  } else {
-    console.error('参数不合法');
+        timer = null;
+      }, timeout);
+    } else {
+      timer = setTimeout(() => {
+        fn.apply(this, arguments);
+        timer = null;
+      }, timeout);
+    }
   }
 }
-
-const test = debounce(() => {
-  console.log('测试');
-}, 500);
 
 
